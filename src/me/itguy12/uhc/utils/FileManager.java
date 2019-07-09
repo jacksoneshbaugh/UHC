ca46cbef5be87112b5eb216b695282c1c02e7b64
Jackson <itguy12games@gmail.com>
2019-07-09 18:50:29 -0400

    Add LoggerManager and FileManager Code

--- src/me/itguy12/uhc/utils/FileManager.java
+++ src/me/itguy12/uhc/utils/FileManager.java
@@ -6,24 +6,99 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.libs.org.apache.commons.io.IOUtils;
 import org.bukkit.plugin.Plugin;
 
 public class FileManager {
-	
+
 	public void onEnable(Plugin p) {
-		//Check for Soft Dependencies, copy them if they don't exist.
+		// Check for Soft Dependencies, copy them if they don't exist.
 		
+	}
+	
+	public void checkDepends(Plugin p) {
 		boolean needsRestart = false;
-		
-		if(!p.getServer().getPluginManager().isPluginEnabled("ActionBarAPI")) {
+
+		if (!p.getServer().getPluginManager().isPluginEnabled("ActionBarAPI")) {
 			needsRestart = true;
-			InputStream in = p.getResource("ActionBarAPI");
+			InputStream in = p.getResource("ActionBarAPI.jar");
+			OutputStream out;
 			try {
-				OutputStream out = new FileOutputStream("../plugins");
+				 out = new FileOutputStream("../plugins");
 			} catch (FileNotFoundException e) {
-				
-				e.printStackTrace();
+				LoggerManager.get().LogException(e);
+				return;
 			}
+
+			try {
+				IOUtils.copy(in, out);
+			} catch (IOException e) {
+				LoggerManager.get().LogException(e);
+				return;
+			}
+		}
+		
+		if (!p.getServer().getPluginManager().isPluginEnabled("ParticleAPI")) {
+			needsRestart = true;
+			InputStream in = p.getResource("ParticleAPI.jar");
+			OutputStream out;
+			try {
+				 out = new FileOutputStream("../plugins");
+			} catch (FileNotFoundException e) {
+				LoggerManager.get().LogException(e);
+				return;
+			}
+
+			try {
+				IOUtils.copy(in, out);
+			} catch (IOException e) {
+				LoggerManager.get().LogException(e);
+				return;
+			}
+		}
+		
+		if (!p.getServer().getPluginManager().isPluginEnabled("TitleAPI")) {
+			needsRestart = true;
+			InputStream in = p.getResource("TitleAPI.jar");
+			OutputStream out;
+			try {
+				 out = new FileOutputStream("../plugins");
+			} catch (FileNotFoundException e) {
+				LoggerManager.get().LogException(e);
+				return;
+			}
+
+			try {
+				IOUtils.copy(in, out);
+			} catch (IOException e) {
+				LoggerManager.get().LogException(e);
+				return;
+			}
+		}
+		
+		if (!p.getServer().getPluginManager().isPluginEnabled("Vault")) {
+			needsRestart = true;
+			InputStream in = p.getResource("Vault.jar");
+			OutputStream out;
+			try {
+				 out = new FileOutputStream("../plugins");
+			} catch (FileNotFoundException e) {
+				LoggerManager.get().LogException(e);
+				return;
+			}
+
+			try {
+				IOUtils.copy(in, out);
+			} catch (IOException e) {
+				LoggerManager.get().LogException(e);
+				return;
+			}
+		}
+		
+		if(needsRestart) {
+			LoggerManager.get().LogInfo("Restarting the server to install dependencies. You may have to start the server again yourself.");
+			Bukkit.getServer().shutdown();
 		}
 	}
 
--- src/me/itguy12/uhc/utils/LoggerManager.java
+++ src/me/itguy12/uhc/utils/LoggerManager.java
@@ -1,5 +1,8 @@
 package me.itguy12.uhc.utils;
 
+import org.bukkit.Bukkit;
+import net.md_5.bungee.api.ChatColor;
+
 public class LoggerManager {
 
 	private static LoggerManager lm;
@@ -11,10 +14,39 @@ public class LoggerManager {
 
 		return lm;
 	}
-	
-	
+
+	public void LogInfo(String s) {
+		Bukkit.getServer().getLogger()
+				.severe(ChatColor.DARK_GRAY + "" + ChatColor.STRIKETHROUGH + "---------------------------"
+						+ ChatColor.GOLD + "" + ChatColor.BOLD + "UHC" + ChatColor.DARK_GRAY + ""
+						+ ChatColor.STRIKETHROUGH + "--------------------------");
+		Bukkit.getServer().getLogger().info(ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "(*) UHC returned some information (*)");
+		Bukkit.getServer().getLogger().info(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + s);
+		Bukkit.getServer().getLogger()
+		.severe(ChatColor.DARK_GRAY + "" + ChatColor.STRIKETHROUGH + "---------------------------"
+				+ ChatColor.GOLD + "" + ChatColor.BOLD + "UHC" + ChatColor.DARK_GRAY + ""
+				+ ChatColor.STRIKETHROUGH + "--------------------------");
+	}
+
 	public void LogException(Exception e) {
-		
+		Bukkit.getServer().getLogger()
+				.severe(ChatColor.DARK_GRAY + "" + ChatColor.STRIKETHROUGH + "---------------------------"
+						+ ChatColor.GOLD + "" + ChatColor.BOLD + "UHC" + ChatColor.DARK_GRAY + ""
+						+ ChatColor.STRIKETHROUGH + "--------------------------");
+		Bukkit.getServer().getLogger()
+				.severe(ChatColor.RED + "" + ChatColor.BOLD + "(!) UHC Encountered an Exception. (!)");
+		Bukkit.getServer().getLogger()
+				.severe(ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "MESSAGE: " + ChatColor.RED + e.getMessage());
+		Bukkit.getServer().getLogger()
+				.severe(ChatColor.RED + "" + ChatColor.BOLD + "If you need help, make an issue on the issue tracker:");
+		Bukkit.getServer().getLogger()
+				.severe(ChatColor.GOLD + "" + ChatColor.BOLD + "https://github.com/JavaGuy12/UHC/issues");
+		Bukkit.getServer().getLogger().severe(ChatColor.RED + "" + ChatColor.BOLD + "Printing the stacktrace below.");
+		e.printStackTrace();
+		Bukkit.getServer().getLogger()
+				.severe(ChatColor.DARK_GRAY + "" + ChatColor.STRIKETHROUGH + "---------------------------"
+						+ ChatColor.GOLD + "" + ChatColor.BOLD + "UHC" + ChatColor.DARK_GRAY + ""
+						+ ChatColor.STRIKETHROUGH + "--------------------------");
 	}
 
 }
